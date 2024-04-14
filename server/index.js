7651f0d83c9524893394c7bb47158c66589f9f59
Erdem <mr.erdembilgin@gmail.com>
2024-04-14 16:07:11 +0300

    connected to mongoDB

---
+++ server/index.js
@@ -0,0 +1,23 @@
+import dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+
+dotenv.config();
+const app = express();
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected To MongoDB!!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.get("/", (req, res) => {
+  res.send("Hello Estate");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
