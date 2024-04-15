6c1640483c2cd8857fdfc7c0bc60cc7e66ade26e
Erdem <mr.erdembilgin@gmail.com>
2024-04-15 17:59:03 +0300

    completed signup page ui

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,7 +6,7 @@ export default function Navbar() {
     <nav className="bg- bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-screen-2xl mx-auto p-3">
         <Link to="/">
-          <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+          <h1 className="font-bold text-sm sm:text-2xl flex flex-wrap">
             <span className="text-slate-500">Bilgin</span>
             <span className="text-slate-700">Estate</span>
           </h1>
@@ -21,17 +21,19 @@ export default function Navbar() {
         </form>
         <ul className="flex gap-4">
           <Link to="/">
-            <li className="hidden sm:inline text-slate-700 hover:underline">
+            <li className="hidden sm:inline text-lg text-slate-700 hover:underline">
               Home
             </li>
           </Link>
           <Link to="/about">
-            <li className="hidden sm:inline text-slate-700 hover:underline">
+            <li className="hidden sm:inline text-lg text-slate-700 hover:underline">
               About
             </li>
           </Link>
           <Link to="/signin">
-            <li className="text-slate-700 hover:underline">Sign in</li>
+            <li className="text-slate-700 text-sm sm:text-lg hover:underline">
+              Sign in
+            </li>
           </Link>
         </ul>
       </div>
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,5 +1,44 @@
-import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Signup() {
-  return <div>Signup</div>;
+  return (
+    <div className="p-3 max-w-2xl mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
+      <form className="flex flex-col gap-4">
+        {/* Username */}
+        <input
+          type="text"
+          id="username"
+          placeholder="Username"
+          className="border p-3 rounded-lg"
+        />
+
+        {/* Email */}
+        <input
+          type="email"
+          id="email"
+          placeholder="Email"
+          className="border p-3 rounded-lg"
+        />
+
+        {/* Password */}
+        <input
+          type="password"
+          id="password"
+          placeholder="Password"
+          className="border p-3 rounded-lg"
+        />
+
+        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
+          sign up
+        </button>
+      </form>
+      <div className="flex gap-2 mt-5 text-lg">
+        <p>Have an account?</p>
+        <Link to="/signin">
+          <span className="text-blue-700">Sign in</span>
+        </Link>
+      </div>
+    </div>
+  );
 }
