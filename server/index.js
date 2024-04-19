ed6a48169c867e5e96e6775846344f1895a71f8c
Erdem <mr.erdembilgin@gmail.com>
2024-04-19 17:46:24 +0300

    created user update api route

--- server/index.js
+++ server/index.js
@@ -3,12 +3,14 @@ import express from "express";
 import mongoose from "mongoose";
 import userRouter from "./router/user.route.js";
 import authRouter from "./router/auth.route.js";
+import cookieParser from "cookie-parser";
 import cors from "cors";
 
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
 
 mongoose
   .connect(process.env.MONGO_URI)
