68d440356b499130360b5c625de159ffde6c3a61
Erdem <mr.erdembilgin@gmail.com>
2024-04-21 15:21:36 +0300

    completed create listing ui

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -6,7 +6,7 @@ import {
   ref,
   uploadBytesResumable,
 } from "firebase/storage";
-
+import { Link } from "react-router-dom";
 import { app } from "../firebase";
 import {
   updateUserStart,
@@ -198,6 +198,12 @@ export default function Profile() {
         >
           {loading ? "Loading..." : "Update"}
         </button>
+        <Link
+          to="/create-listing"
+          className="bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95"
+        >
+          Create Listing
+        </Link>
       </form>
       <div className="flex justify-between mt-4">
         <span
