50bfac0f980e822b8e92726c5258d7776e92d7b6
Erdem <mr.erdembilgin@gmail.com>
2024-04-24 17:41:14 +0300

    Completed delete user listing functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -147,6 +147,25 @@ export default function Profile() {
     }
   };
 
+  const handleListingDelete = async (id) => {
+    try {
+      const res = await fetch(`/api/listing/delete/${id}`, {
+        method: "DELETE",
+      });
+
+      const data = await res.json();
+
+      if (data.success == false) {
+        console.log(data.message);
+        return;
+      }
+
+      setUserListings((prev) => prev.filter((listing) => listing._id !== id));
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+
   return (
     <div className="p-3 max-w-2xl mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -274,7 +293,12 @@ export default function Profile() {
               </Link>
               <div className="flex flex-col gap-1 items-center">
                 <button className="text-green-700 uppercase">EDIT</button>
-                <button className="text-red-700 uppercase">DELETE</button>
+                <button
+                  onClick={() => handleListingDelete(listing._id)}
+                  className="text-red-700 uppercase"
+                >
+                  DELETE
+                </button>
               </div>
             </div>
           ))}
--- server/controllers/listing.controller.js
+++ server/controllers/listing.controller.js
@@ -1,9 +1,27 @@
 import Listing from "../models/listing.model.js";
+import { errorHandler } from "../utils/error.js";
 
 export const createListing = async (req, res, next) => {
   try {
     const listing = await Listing.create(req.body);
     return res.status(201).json(listing);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const deleteListing = async (req, res, next) => {
+  const { id } = req.params;
+  const listing = await Listing.findById(id);
+
+  if (!listing) return next(errorHandler(404, "Listing could not found!"));
+
+  if (req.user.id !== listing.userRef)
+    return next(errorHandler(401, "You can only delete your own listings!"));
+
+  try {
+    await Listing.findByIdAndDelete(id);
+    return res.status(200).json({ message: "Listing has been deleted" });
   } catch (err) {
     next(err);
   }
