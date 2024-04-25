1d608f466661b26ce89a39d3362a775be49de92a
Erdem <mr.erdembilgin@gmail.com>
2024-04-25 20:55:08 +0300

    Created search api route

--- server/router/listing.route.js
+++ server/router/listing.route.js
@@ -4,6 +4,7 @@ import {
   deleteListing,
   updateListing,
   getListing,
+  getListings,
 } from "../controllers/listing.controller.js";
 import { verifyToken } from "../utils/verifyToken.js";
 
@@ -12,6 +13,8 @@ const router = express.Router();
 router.post("/create", verifyToken, createListing);
 router.delete("/delete/:id", verifyToken, deleteListing);
 router.post("/update/:id", verifyToken, updateListing);
+router.get("/get", getListings);
+
 router.get("/:id", getListing);
 
 export default router;
